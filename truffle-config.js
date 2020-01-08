62bc53ed916fb6bf85979e996eec751e1adfaaa5
Divyanth Jayaraj <divyanth@mudrex.com>
2020-01-08 15:16:43 +0530

    Added jur smart contracts

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,16 @@
-const path = require("path");
-
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "app/src/contracts"),
   networks: {
-    develop: {
-      port: 8545
+    development: {
+      host: "localhost",
+      port: 8545,
+      gas: 6721975,
+      network_id: "*" // Match any network id
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
     }
   }
 };
