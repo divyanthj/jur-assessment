597ad64a184d2231b5444f2687e60972cbb2ff54
Divyanth Jayaraj <divyanth@mudrex.com>
2020-01-09 13:04:39 +0530

    Fixed truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,16 +1,11 @@
 module.exports = {
+  contracts_build_directory: path.join(__dirname, "app/src/contracts"),
   networks: {
     develop: {
       host: "127.0.0.1",
       port: 7545,
       gas: 6721975,
       network_id: "*" // Match any network id
-    }
-  },
-  solc: {
-    optimizer: {
-      enabled: true,
-      runs: 200
     }
   }
 };
