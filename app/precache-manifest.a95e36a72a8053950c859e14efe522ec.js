acc3b38d8bd2041c68e7aed16cfc5698a27d7dcb
Divyanth Jayaraj <divyanth@mudrex.com>
2020-01-12 01:41:46 +0530

    Demo

---
+++ app/precache-manifest.a95e36a72a8053950c859e14efe522ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67801bd9b70b30723b4ea96587a2cca1",
+    "url": "localhost:8080/index.html"
+  },
+  {
+    "revision": "9f87f8d45d2bebd7193e",
+    "url": "localhost:8080/static/css/main.60e60b23.chunk.css"
+  },
+  {
+    "revision": "43623559b6f0282e1d49",
+    "url": "localhost:8080/static/js/2.bef05ead.chunk.js"
+  },
+  {
+    "revision": "9f87f8d45d2bebd7193e",
+    "url": "localhost:8080/static/js/main.2cb90ce5.chunk.js"
+  },
+  {
+    "revision": "9fab76d805d3b6cadbb6",
+    "url": "localhost:8080/static/js/runtime-main.74cc220f.js"
+  }
+]);
